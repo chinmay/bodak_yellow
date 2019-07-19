42722ee0ef308fbcc809272090c5867e18322780
Chinmay <craval@ripple.com>
2019-07-19 07:34:58 -0700

    Adding XRPL calls

--- api/app.js
+++ api/app.js
@@ -1,8 +1,21 @@
+const RippleAPI = require('ripple-lib').RippleAPI;
 var express = require("express");
 var bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 
 var app = express();
 var port = process.env.PORT || 8080;
+
+const api = new RippleAPI({
+    // server: 'wss://s1.ripple.com' // Public rippled server
+    server: 'wss://s.altnet.rippletest.net:51233' // Test Net
+});
+
+api.connect().then(async () => {
+    console.log('Connected to rippled');
+});
+
+const marketCoordinatorAccount = api.generateAddress();
 
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -20,27 +33,33 @@ app.get("/team", (req, res, next) => {
 });
 
 app.post('/api/borrow', function (req, res) {
+    var account = api.generateAddress();
     var obj = {}
     obj.id = create_UUID();
     obj.request_type = 'borrow';
     obj.amount = req.body.amount;
     obj.currency = req.body.currency;
     obj.interest_rate = req.body.interest_rate;
+    obj.address = account.address;
+    obj.status = 'CREATED';
 
     borrowers.set(obj.id, obj);
-    res.json(obj)
+    res.json(Object.assign({}, obj, { secret: account.secret }));
 });
 
 app.post('/api/lend', function (req, res) {
+    var account = api.generateAddress();
     var obj = {}
     obj.id = create_UUID();
     obj.request_type = 'lend';
     obj.amount = req.body.amount;
     obj.currency = req.body.currency;
     obj.interest_rate = req.body.interest_rate;
+    obj.address = account.address;
+    obj.status = 'CREATED';
 
     lenders.set(obj.id, obj);
-    res.json(obj)
+    res.json(Object.assign({}, obj, { secret: account.secret }));
 });
 
 app.get("/api/borrowers", (req, res, next) => {
@@ -51,38 +70,92 @@ app.get("/api/lenders", (req, res, next) => {
     res.json(Array.from(lenders));
 });
 
-app.get("/api/match/lender", (req, res, next) => {
+app.get("/api/match/lender", async (req, res, next) => {
     // you are borrower, searching for a matching lender
     var borrower_id = req.body.id; // should be a borrower_id
     borrower = borrowers.get(borrower_id);
     var matching_amount = borrower.amount;
     console.log("find lender with amount: ", matching_amount);
     var match = {};
+    var result = {};
     for (var value of lenders.values()) {
-        //console.log(value);
         if (matching_amount === value.amount) {
             match = value;
+            result = await setUpMultisignAccount(borrower, value);
+            break;
         }
     }
-    res.json(match);
+    res.json(Object.assign({}, match, result));
 });
 
 
-app.get("/api/match/borrower", (req, res, next) => {
+app.get("/api/match/borrower", async (req, res, next) => {
     // you are lender, searching for a matching borrower
     var lender_id = req.body.id; // should be a lender_id
-    lender = borrowers.get(lender_id);
+    lender = lenders.get(lender_id);
     var matching_amount = lender.amount;
     console.log("find borrower with amount: ", matching_amount);
     var match = {};
+    var result = {};
     for (var value of borrowers.values()) {
-        //console.log(value);
         if (matching_amount === value.amount) {
             match = value;
+            result = await setUpMultisignAccount(value, lender);
+            break;
         }
     }
-    res.json(match);
+    res.json(Object.assign({}, match, result));
 });
+
+app.get("/api/account", async (req, res, next) => {
+    var address = req.body.account; // should be a multi-sign account_address from ledger
+    result = await api.getAccountInfo(address)
+    res.json(result);
+});
+
+async function setUpMultisignAccount(borrower, lender) {
+    const response = await fetch('https://faucet.altnet.rippletest.net/accounts', { method: 'POST' });
+    const account = (await response.json()).account;
+    console.log(account);
+    borrower.status = 'MATCHED';
+    lender.status = 'MATCHED';
+    borrower.collateral_account = account.address;
+    lender.collateral_account = account.address;
+    await sleep(8000);
+    const prepared = await api.prepareSettings(account.address, {
+        signers: {
+            threshold: 2,
+            weights: [
+                {
+                    address: marketCoordinatorAccount.address,
+                    weight: 1
+                },
+                {
+                    address: borrower.address,
+                    weight: 1
+                },
+                {
+                    address: lender.address,
+                    weight: 1
+                }
+            ]
+        }
+    });
+    //console.log('txJSON:', prepared.txJSON);
+    const signed = api.sign(prepared.txJSON, account.secret);
+    //console.log(signed);
+    const result = await api.submit(signed.signedTransaction);
+    console.log(result);
+    return result;
+}
+
+async function sleep(timeout) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve();
+        }, timeout);
+    });
+}
 
 function create_UUID() {
     var dt = new Date().getTime();
@@ -93,27 +166,3 @@ function create_UUID() {
     });
     return uuid;
 }
-
-// ripple-lib example
-const RippleAPI = require('ripple-lib').RippleAPI;
-
-const api = new RippleAPI({
-    server: 'wss://s1.ripple.com' // Public rippled server
-});
-api.connect().then(() => {
-    /* begin custom code ------------------------------------ */
-    const myAddress = 'rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn';
-
-    console.log('getting account info for', myAddress);
-    return api.getAccountInfo(myAddress);
-
-}).then(info => {
-    console.log(info);
-    console.log('getAccountInfo done');
-
-    /* end custom code -------------------------------------- */
-}).then(() => {
-    return api.disconnect();
-}).then(() => {
-    console.log('done and disconnected.');
-}).catch(console.error);
